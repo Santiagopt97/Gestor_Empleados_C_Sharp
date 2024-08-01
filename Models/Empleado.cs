d24a7dfa84ba2e9c7383441a0826cace2465d2cd
Santiagopt97 <santiagopineda99@gmail.com>
2024-08-01 09:27:59 -0500

    fix: elimine algunos atributos para traerlos con herencia de Persona

--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -5,17 +5,12 @@ using System.Threading.Tasks;
 
 namespace Gestor_Empleados.Models
 {
-    public class Empleado
-    {
-        public  Guid Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string NumeroDeIdentificacion { get; set; }
-        public byte Edad { get; set; }
+    public class Empleado : Persona
+    {        
         public string Posicion { get; set; }
-        public double Salario { get; set; }
+        public decimal Salario { get; set; }
 
-        public Empleado(string nombre, string apellido, string numeroDeIdentificacion, byte edad, string posicion, double salario)
+        public Empleado(string nombre, string apellido, string numeroDeIdentificacion, byte edad, string posicion, decimal salario)
         {
             Id = Guid.NewGuid();
             Nombre = nombre.ToLower();
@@ -26,13 +21,13 @@ namespace Gestor_Empleados.Models
             Salario = salario;
         }
 
-        private double CalcularBonificacion(){
-            var bonificacion = Salario * 0.10;
+        private decimal CalcularBonificacion(){
+            var bonificacion = Salario * Convert.ToDecimal(0.10);
             var salarioConBonificacion = Salario + bonificacion;
             return salarioConBonificacion;
         }
 
-        public void MostrarInformacion(){
+        public override void MostrarInformacion(){
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("--------Informacion del empleado--------");
             Console.WriteLine("----------------------------------------");
@@ -45,4 +40,4 @@ namespace Gestor_Empleados.Models
             Console.WriteLine("----------------------------------------");
         }
     }
-}
+}   
